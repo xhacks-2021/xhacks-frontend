616f055bb9dd0635c613ccf462f8397582eaa0b7
Emily Y <emilyyst19@hotmail.com>
2021-08-07 18:01:20 -0400

    Connect to relevance api
    Added front-end design and connected to relevance api
    
    Co-Authored-By: Max Huang <max.huang@uwaterloo.ca>

--- index.js
+++ index.js
@@ -1,58 +1,105 @@
-function fetching() {
-    let filelist = document.getElementById('avatar').files;
+// called when a file is uploaded; parses the text and sends requests to the apis
+function start() {
+  let filelist = document.getElementById('avatar').files;
+  let pdfjsLib = window['pdfjs-dist/build/pdf'];
+  pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://mozilla.github.io/pdf.js/build/pdf.worker.js';
 
-    // Loaded via <script> tag, create shortcut to access PDF.js exports.
-    let pdfjsLib = window['pdfjs-dist/build/pdf'];
+  let fileReader = new FileReader();
+  fileReader.onload = function () {
+    let typedarray = new Uint8Array(this.result);
+    const loadingTask = pdfjsLib.getDocument(typedarray);
+    loadingTask.promise.then(pdf => {
 
-    // The workerSrc property shall be specified.
-    pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://mozilla.github.io/pdf.js/build/pdf.worker.js';
+      var pagesPromises = [];
 
-    let fileReader = new FileReader();
+      for (var i = 0; i < pdf.numPages; i++) {
+        // Required to prevent that i is always the total of pages
+        (function (pageNumber) {
+          pagesPromises.push(getPageText(pageNumber, pdf));
+        })(i + 1);
+      }
+      Promise.all(pagesPromises).then(function (pagesText) {
+        // join page strings
+        pagesText = pagesText.join(' ');
 
-    fileReader.onload = function () {
+        // Display text of all the pages in the console
+        console.log(pagesText);
 
-        let typedarray = new Uint8Array(this.result);
-        const loadingTask = pdfjsLib.getDocument(typedarray);
+        // TODO: call question api
+        let questions = {
+          "questions": [
+            {
+              "answer": "Immanuel Kant",
+              "question": "Who is the central figure in modern philosophy?"
+            },
+            {
+              "answer": "1724–1804",
+              "question": "When was Immanuel Kant born?"
+            },
+            {
+              "answer": "rationalism and empiricism",
+              "question": "What did Kant synthesize in early modern philosophy?"
+            }
+          ],
+          "text": "Immanuel Kant (1724–1804) is the central figure in modern philosophy. He synthesized early modern rationalism and empiricism, set the terms for much of nineteenth and twentieth century philosophy, and continues to exercise a significant influence today in metaphysics, epistemology, ethics, political philosophy, aesthetics, and other fields."
+        };
 
-        // Get Document
-        pdfjsLib.getDocument(typedarray).then(function (pdf) {
+        // call relevance api
+        post("https://xhacks-2021.herokuapp.com/is-relevant", questions);
+      });
 
-            let pdfDoc = pdf;
-            let pagesPromises = [];
-            // for(let i = 0; i < pdf.numPages; i++){
-            //     //Convert to string
-            //     //console.log()
-            //         pagesPromises.push(getPageText(pageNumber, pdfDocument));       
-            // }
-            console.log(pdf.numPages);
-        });
-        pdfjsLib.getDocument(typedarray).promise.catch(function (err) {
+    }, function (reason) {
+      // PDF loading error
+      console.error(reason);
+    });
+  }
+  fileReader.readAsArrayBuffer(filelist[0]);
+}
 
-            //Display error
-            const div = pdfjsLib.createElement('div');
-            div.className = 'error';
-            div.appendChild(pdfjsLib.createTextNode(err.message));
-            pdfjsLib.querySelector('body').insertBefore(div, canvas);
 
-        });
+/**
+ * Retrieves the text of a specif page within a PDF Document obtained through pdf.js 
+ * 
+ * @param {Integer} pageNum Specifies the number of the page 
+ * @param {PDFDocument} PDFDocumentInstance The PDF document obtained 
+ **/
+function getPageText(pageNum, PDFDocumentInstance) {
+  // Return a Promise that is solved once the text of the page is retrieven
+  return new Promise(function (resolve, reject) {
+    PDFDocumentInstance.getPage(pageNum).then(function (pdfPage) {
+      // The main trick to obtain the text of the PDF page, use the getTextContent method
+      pdfPage.getTextContent().then(function (textContent) {
+        var textItems = textContent.items;
+        var finalString = "";
 
-        function getPageText(pageNum, PDFDocumentInstance) {
-            return new Promise(function (resolve, reject) {
-                PDFDocumentInstance.getPage(pageNum).then(function (pdfPage) {
-                    // The main trick to obtain the text of the PDF page, use the getTextContent method
-                    pdfPage.getTextContent().then(function (textContent) {
-                        var textItems = textContent.items;
-                        var finalString = "";
-                        // Concatenate the string of the item to the final string
-                        for (var i = 0; i < textItems.length; i++) {
-                            var item = textItems[i];
-                            finalString += item.str + " ";
-                        }
-                        // Solve promise with the text retrieven from the page
-                        resolve(finalString);
-                    });
-                });
-            });
+        // Concatenate the string of the item to the final string
+        for (var i = 0; i < textItems.length; i++) {
+          var item = textItems[i];
+
+          finalString += item.str + " ";
         }
-    }
+
+        // Solve promise with the text retrieven from the page
+        resolve(finalString);
+      });
+    });
+  });
+}
+
+// sends a post request using an endpoint and json body
+function post(endpoint, body) {
+  fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 }
